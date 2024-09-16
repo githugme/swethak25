c7e997797ab90d0e344d23e2f5631602c845f3a2
githugme <77107059+githugme@users.noreply.github.com>
2024-09-16 23:42:09 +0530

    Update index.js
    Edited with love. new branch

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-alert();
-alert("brakess!\nIf the song is not playing on the background,\ncheck the top left corner and hit the play button.");
-alert("Enjoy every bit of it.");
+alert("Hi Swetha Kalyan Krishnan..");
+alert("just a gentle reminder!\nIf the song is not playing on the background,\ncheck the top left corner and hit the play button.");
+alert("made with love.. just for you");
 window.addEventListener("DOMContentLoaded", event => {
   const audio = document.querySelector("audio");
   audio.volume = 1;
